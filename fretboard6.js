2d093b1b2b80efefcd8b2ad71af23859e34d13f7
uduk <iherlambang@gmail.com>
2016-06-03 11:03:15 +0700

    Add files via upload

--- fretboard6.js
+++ fretboard6.js
@@ -1,218 +1,181 @@
+/*
 
-/**
-*
-* UDUK FretBoard (6) 1.0
-* require: uduk-core paperjs
-*
+o   o o-o   o   o o  o 
+|   | |  \  |   | | /  
+|   | |   O |   | OO   
+|   | |  /  |   | | \  
+ o-o  o-o    o-o  o  o  FretBoard
+
 */
 
-var posX = 100;
-var posY = 100;
-
-UdukFretboard6 = function(x, y) {
+UdukFretboard6 = function(x, y, tuning) {
 
   var width = 960;
   var height = 150;
-
-  this.posX = x;
-  this.posY = y;
+  posX = x;
+  posY = y;
 
   /* Rectangle */
-	var rectangle = new Rectangle(new Point(this.posX, this.posY), new Size(width, height));
-	var shape = new Shape.Rectangle(rectangle);
-	shape.fillColor = '#ffeeb1';
-	shape.strokeColor = 'black';
+  var rectangle = new Rectangle(new Point(posX, posY), new Size(width, height));
+  var shape = new Shape.Rectangle(rectangle);
+  shape.fillColor = '#ffeeb1';
+  shape.strokeColor = 'black';
 
   /* Nut */
-	var nut = new Rectangle(new Point(this.posX-16, this.posY), new Size(16, height));
-	var nut1 = new Shape.Rectangle(nut);
-	nut1.fillColor = 'grey';
-	nut1.strokeColor = 'black';
+  var rnut = new Rectangle(new Point(posX-16, posY), new Size(16, height));
+  var nut = new Shape.Rectangle(rnut);
+  nut.fillColor = 'grey';
+  nut.strokeColor = 'black';
 
-	/* last */
-	var rectangle3 = new Rectangle(new Point(width+this.posX, this.posY), new Size(6, height));
-	var shape3 = new Shape.Rectangle(rectangle3);
-	shape3.fillColor = 'grey';
-	shape3.strokeColor = 'black';
+  /* Last Nut */
+  var rrnut = new Rectangle(new Point(width+posX, posY), new Size(6, height));
+  var lnut = new Shape.Rectangle(rrnut);
+  lnut.fillColor = 'grey';
+  lnut.strokeColor = 'black';
 
-	/* fret */
-	for(i = 1; i < 24; i++) {
-		x = (i * 40) + this.posX;
-		var s = new Path.Line({
-				from: [x, this.posY],
-				to: [x, height+this.posY],
-				strokeColor: 'black'
-		});
-	}
+  /* Fret */
+  for (var i = 1; i < 24; i++) {
+    x = (i * 40) + posX;
+    var s = new Path.Line({
+      from: [x, posY],
+      to: [x, height+posY],
+      strokeColor: 'black'
+    });
+  }
 
-  /* fret number */
-	var z = 20 + this.posX;
-	for(i = 1; i <= 24; i++, z += 40) {
+  /* Fret number */
+  var z = 20 + posX;
+  for (var i = 1; i <= 24; i++, z += 40) {
+    /* Bottom */
+    var bottom = new PointText(new Point(z, posY + height + 30));
+    bottom.justification = 'center';
+    bottom.fontFamily = 'Silom';
+    bottom.fillColor = 'black';
+    bottom.content = i + '';
 
-	  // Bottom
-		var text = new PointText(new Point(z, this.posY + height + 30));
-		text.justification = 'center';
-		text.fontFamily = 'Silom';
-		text.fillColor = 'black';
-		text.content = i + '';
+    /* Top */
+    var text2 = new PointText(new Point(z, posY - 20));
+    text2.justification = 'center';
+    text2.fontFamily = 'Silom';
+    text2.fillColor = 'black';
+    text2.content = i + '';
+  }
 
-		// Top
-		var text2 = new PointText(new Point(z, this.posY - 20));
-		text2.justification = 'center';
-		text2.fontFamily = 'Silom';
-		text2.fillColor = 'black';
-		text2.content = i + '';
-	}
+  /* String */
+  for (var i = 1; i <= 4; i++) {
+    var y = i * 30;
+    var s = new Path.Line({
+      from: [0+posX, y+posY],
+      to: [width+posX, y+posY],
+      strokeColor: 'grey'
+    });
+  }
 
-	/* string */
-	for(i = 1; i <= 4; i++) {
-		var y = i * 30;
-		var s = new Path.Line({
-				from: [0+this.posX, y+this.posY],
-				to: [width+this.posX, y+this.posY],
-				strokeColor: 'grey'
-		});
-	}
+  /* String tuning */
+  for (var i = 0; i < tuning.length; i++) {
+    var y = i * 30;
+    var text = new PointText(new Point(posX - 40, (y+posY) - 30));
+    text.justification = 'center';
+    text.fontFamily = 'Silom';
+    text.fillColor = 'black';
+    text.content = tuning[i];
+  }
 
-  /* string tuning */
-	var tuning = ["-", "e", "B", "G", "D", "A", "E"];
-	for(i = 1; i <= 6; i++) 
-	{
-		var y = i * 30;
-		var text = new PointText(new Point(this.posX - 40, (y+this.posY) - 30 ));
-		text.justification = 'center';
-		text.fontFamily = 'Silom';
-		//text.fontWeight = 'bold';
-		text.fillColor = 'black';
-		text.content = tuning[i];
-	}
+  // dots
+  var d1 = 100;
+  var d2 = 580;
+  for (var i = 1; i < 5; i++, d1 += 80, d2 += 80) {
+    // 3, 5, 7, 9
+    var myCircle = new Path.Circle(new Point(d1+posX, 76+posY), 10);
+    myCircle.fillColor = 'grey';
 
-	// dots
-	var d1 = 100;
-	var d2 = 580;
-	for(i = 1; i < 5; i++, d1 += 80, d2 += 80)
-	{
-	  // 3, 5, 7, 9
-		var myCircle = new Path.Circle(new Point(d1+this.posX, 76+this.posY), 10);
-		myCircle.fillColor = 'grey';
+    // 15. 17, 19, 21
+    var myCircle = new Path.Circle(new Point(d2+posX, 76+posY), 10);
+    myCircle.fillColor = 'grey';
+  }
 
-		// 15. 17, 19, 21
-		var myCircle = new Path.Circle(new Point(d2+this.posX, 76+this.posY), 10);
-		myCircle.fillColor = 'grey';
-	}
+    // 12
+    var myCircle = new Path.Circle(new Point(460+posX, 46+posY), 10);
+    myCircle.fillColor = 'grey';
+    var myCircle = new Path.Circle(new Point(460+posX, 106+posY), 10);
+    myCircle.fillColor = 'grey';
 
-  // 12
-	var myCircle = new Path.Circle(new Point(460+this.posX, 46+this.posY), 10);
-	myCircle.fillColor = 'grey';
-	var myCircle = new Path.Circle(new Point(460+this.posX, 106+this.posY), 10);
-	myCircle.fillColor = 'grey';
+    //	24
+    var myCircle = new Path.Circle(new Point(940+posX, 46+posY), 10);
+    myCircle.fillColor = 'black';
+    var myCircle = new Path.Circle(new Point(940+posX, 106+posY), 10);
+    myCircle.fillColor = 'black';
 
-	//	24
-	var myCircle = new Path.Circle(new Point(940+this.posX, 46+this.posY), 10);
-	myCircle.fillColor = 'black';
-	var myCircle = new Path.Circle(new Point(940+this.posX, 106+this.posY), 10);
-	myCircle.fillColor = 'black';
-
-	// Powered: 
-	var text = new PointText(80, 20);
-	text.content = 'http://uduk.org';
-	text.style = {
-						fontFamily: 'Silom',
-						fontWeight: 'bold',
-						fontSize: 16,
-						fillColor: 'grey',
-						justification: 'center'
-	};
+  // Powered: 
+  var uduk = new PointText(80, 20);
+  uduk.content = 'http://uduk.org';
+  uduk.style = {
+    fontFamily: 'Silom',
+    fontWeight: 'bold',
+    fontSize: 16,
+    fillColor: 'grey',
+    justification: 'center'
+  };
+  
+  var ret = [posX, posY];
+  return ret;
 
 };
 
-// todo: range check
-drawNote6 = function(s, f, marker) {
+drawNote6 = function(pos, s, f, marker) {
+  var string = pos[1] + (s - 1) * 30;
+  var fret = pos[0] + 20 + (f - 1) * 40;
 
-  // strings --- this.posY + (i - 1) * 30
-	// 1 = this.posY + 0  
-	// 2 = this.posY + 30 
-	// 3 = this.posY + 60 
-	// 4 = this.posY + 90 
-	// 5 = this.posY + 120
-	// 6 = this.posY + 150
+  var circle = new Path.Circle(new Point(fret, string), 8);
+    circle.style = {
+    fillColor: '#ff6060',
+    strokeColor: 'black',
+    strokeWidth: 1,
+    shadowColor: new Color(0, 0, 0),
+    shadowBlur: 12,
+    shadowOffset: new Point(5, 5)
+  };
 
-	// fret -- this.posX + 20 + (i - 1) *  40
-	// 0 -
-	// 1 - this.posX + 20
-	// 2 - this.posX + 20 + 40
-
-	var string = this.posY + (s - 1) * 30;
-	var fret = this.posX + 20 + (f - 1) * 40;
-
-	var circle = new Path.Circle(new Point(fret, string), 8);
-	circle.style = {
-			fillColor: '#ff6060',
-			strokeColor: 'black',
-			strokeWidth: 1,
-      shadowColor: new Color(0, 0, 0),
-      shadowBlur: 12,
-      shadowOffset: new Point(5, 5)
-	};
-
-	// marker
-	var text = new PointText(new Point(fret, string+4));
-		text.justification = 'center';
-		text.fillColor = 'white';
-		text.content = marker;
+  var text = new PointText(new Point(fret, string+4));
+  text.justification = 'center';
+  text.fillColor = 'white';
+  text.content = marker;
 
   var group = new Group([circle, text]);
   return group;
 };
 
-drawSeq6 = function(seq, marker) {
+drawSeq6 = function(pos, seq, marker) {
+  var z = UdukSequence.splitNote(seq);
+  var s = z[0];
+  var f = z[1]; 
 
-  // strings --- this.posY + (i - 1) * 30
-	// 1 = this.posY + 0  
-	// 2 = this.posY + 30 
-	// 3 = this.posY + 60 
-	// 4 = this.posY + 90 
-	// 5 = this.posY + 120
-	// 6 = this.posY + 150
+  var string = pos[1] + (s - 1) * 30;
+  var fret = pos[0] + 20 + (f - 1) * 40;
 
-	// fret -- this.posX + 20 + (i - 1) *  40
-	// 0 -
-	// 1 - this.posX + 20
-	// 2 - this.posX + 20 + 40
+  var circle = new Path.Circle(new Point(fret, string), 8);
+    circle.style = {
+    fillColor: '#ff6060',
+    strokeColor: 'black',
+    strokeWidth: 1,
+    shadowColor: new Color(0, 0, 0),
+    shadowBlur: 12,
+    shadowOffset: new Point(5, 5)
+  };
 
-	var z = UdukSequence.splitNote(seq);
-	var s = z[0];
-	var f = z[1]; 
-
-	var string = this.posY + (s - 1) * 30;
-	var fret = this.posX + 20 + (f - 1) * 40;
-
-	var circle = new Path.Circle(new Point(fret, string), 8);
-	circle.style = {
-			fillColor: '#ff6060',
-			strokeColor: 'black',
-			strokeWidth: 1,
-      shadowColor: new Color(0, 0, 0),
-      shadowBlur: 12,
-      shadowOffset: new Point(5, 5)
-	};
-
-
-	// marker
-	var text = new PointText(new Point(fret, string+4));
-		text.justification = 'center';
-		text.fillColor = 'white';
-		text.content = marker;
+  var text = new PointText(new Point(fret, string+4));
+  text.justification = 'center';
+  text.fillColor = 'white';
+  text.content = marker;
 
   var group = new Group([circle, text]);
   return group;
 };
 
-drawBlock6 = function(start, n) {
-  var r = new Rectangle(new Point(this.posX+(40 * (start-1)), this.posY), new Size(40 * n, 150));
-	var block = new Shape.Rectangle(r);
-	block.fillColor = '#0f6060';
-	block.opacity = 0.16;
+drawBlock6 = function(pos, start, n) {
+  var r = new Rectangle(new Point(pos[0]+(40 * (start-1)), pos[1]), new Size(40 * n, 150));
+  var block = new Shape.Rectangle(r);
+  block.fillColor = '#0f6060';
+  block.opacity = 0.16;
   return block;
 };
-
